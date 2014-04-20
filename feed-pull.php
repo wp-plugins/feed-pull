c02a3b1523500f1da8d2aeb323ff923c1440c493
tlovett1 <tlovett1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-20 20:06:03 +0000

    Version 0.1.5
    git-svn-id: https://plugins.svn.wordpress.org/feed-pull/tags/0.1.5@898888 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feed-pull.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Feed Pull
+ * Plugin URI: http://www.taylorlovett.com
+ * Description: Automatically turn feed content into posts.
+ * Author: Taylor Lovett
+ * Version: 0.1.5
+ * Author URI: http://www.taylorlovett.com
+ */
+
+define( 'FP_OPTION_NAME', 'fp_feed_pull' );
+
+require_once( dirname( __FILE__ ) . '/includes/class-fp-feed-pull.php' );
